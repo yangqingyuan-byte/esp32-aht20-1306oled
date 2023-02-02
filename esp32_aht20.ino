fa52fea9698e9c2adb57382fb1b882a73df115aa
yangqingyuan-byte <72181431+yangqingyuan-byte@users.noreply.github.com>
2023-02-03 01:52:09 +0800

    Add files via upload

---
+++ esp32_aht20.ino
@@ -0,0 +1,165 @@
+#include <Arduino.h>
+#include <U8g2lib.h>
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 16, /* data=*/ 17, /* reset=*/ U8X8_PIN_NONE);   // ESP32 Thing, pure SW emulated I2C
+
+
+
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
+#include "DFRobot_AHT20.h"
+DFRobot_AHT20 aht20;
+
+// 在这里填写你要链接的WiFi信息
+const char* ssid = "USTC";
+const char* password = "20011013a";
+
+AsyncWebServer server(80);
+
+String readAHTTemperature() {
+  float t = aht20.getTemperature_C();
+  if (isnan(t)) {
+    Serial.println("Failed to read from AHT sensor!");
+    return "--";
+  }
+  else {
+    Serial.println(t);
+    return String(t);
+  }
+}
+
+String readAHTHumidity() {
+
+  float h = aht20.getHumidity_RH();
+  if (isnan(h)) {
+    Serial.println("Failed to read from AHT sensor!");
+    return "--";
+  }
+  else {
+    Serial.println(h);
+    return String(h);
+  }
+}
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <style>
+    html {
+     font-family: Arial;
+     display: inline-block;
+     margin: 0px auto;
+     text-align: center;
+    }
+    h2 { font-size: 3.0rem; }
+    p { font-size: 3.0rem; }
+    .units { font-size: 1.2rem; }
+    .dht-labels{
+      font-size: 1.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+  </style>
+</head>
+<body>
+  <h2>ESP32 AHT20</h2>
+  <p>
+    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i>
+    <span class="dht-labels">Temperature</span>
+    <span id="temperature">%TEMPERATURE%</span>
+    <sup class="units">&deg;C</sup>
+  </p>
+  <p>
+    <i class="fas fa-tint" style="color:#00add6;"></i>
+    <span class="dht-labels">Humidity</span>
+    <span id="humidity">%HUMIDITY%</span>
+    <sup class="units">&percnt;</sup>
+  </p>
+</body>
+<script>
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("temperature").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/temperature", true);
+  xhttp.send();
+}, 10000 ) ;
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("humidity").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/humidity", true);
+  xhttp.send();
+}, 10000 ) ;
+</script>
+</html>)rawliteral";
+
+
+String processor(const String& var){
+  if(var == "TEMPERATURE"){
+    return readAHTTemperature();
+  }
+  else if(var == "HUMIDITY"){
+    return readAHTHumidity();
+  }
+  return String();
+}
+
+void setup(){
+
+  Serial.begin(115200);
+
+  aht20.begin();
+  
+  u8g2.begin();
+
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+
+  Serial.println(WiFi.localIP());
+
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readAHTTemperature().c_str());
+  });
+  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readAHTHumidity().c_str());
+  });
+
+  server.begin();
+}
+
+void loop()
+{
+  aht20.startMeasurementReady(/* crcEn = */true);
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_ncenB08_tr);
+  u8g2.setFontDirection(0);
+  u8g2.setCursor(0, 15);
+  u8g2.print("Temperature:"+String(aht20.getTemperature_C())+"C");
+  u8g2.setCursor(0, 40);
+  u8g2.print("Humidity:"+String(aht20.getHumidity_RH())+"%");
+  u8g2.sendBuffer(); 
+}
